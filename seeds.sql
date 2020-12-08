cb2699a22cd5b4b24d02149c9fe5b82834b3bef3
David Dugle <david.dugleii@yahoo.com>
2020-12-07 19:42:05 -0500

    setting up the dependencies and JSON

---
+++ seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO department (name) values ("Sales");
+INSERT INTO department (name) values ("Engineering");
+INSERT INTO department (name) values ("Finance");
+INSERT INTO department (name) values ("Legal");
+
